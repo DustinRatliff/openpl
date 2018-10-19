20075cf13f9a207a7e9362427957ca3d6a76c1e3
Dustin Ratliff <dustin.ratliff@gmail.com>
2018-10-19 16:01:37 -0400

    Commit from R Studio

--- analysis.R
+++ analysis.R
@@ -2,27 +2,41 @@ library(tidyverse)
 
 openpl <- read_csv("data/openpowerlifting.csv")
 
-openpl %>%
-        select(-"Squat1Kg", -"Squat2Kg", -"Squat3Kg", -"Squat4Kg", -"Bench1Kg", -"Bench2Kg", -"Bench3Kg", -"Bench4Kg", -"Deadlift1Kg", -"Deadlift2Kg", -"Deadlift3Kg", -"Deadlift4Kg", -"TotalKg", -"Wilks", -"McCulloch", -"Glossbrenner"
+openpl_tidy <- openpl %>%
+        select(-"Squat1Kg", -"Squat2Kg", -"Squat3Kg", -"Squat4Kg", -"Bench1Kg", -"Bench2Kg", -"Bench3Kg", -"Bench4Kg", -"Deadlift1Kg", -"Deadlift2Kg", -"Deadlift3Kg", -"Deadlift4Kg", -"Wilks", -"McCulloch", -"Glossbrenner"
         ) %>% 
         rename(Squat = Best3SquatKg) %>%
         rename(Bench = Best3BenchKg) %>%
         rename(Deadlift = Best3DeadliftKg) %>%
+        rename(Total = TotalKg) %>%
 gather(key = Lift,
-       value = LiftKG,
+       value = LiftKg,
        Squat,
        Bench,
-       Deadlift)
+       Deadlift,
+       Total) %>%
+        mutate(LiftKg = replace(LiftKg, LiftKg < 0 , NA))
 
-        
-        
-        
-        
-        filter(tested == "Yes") %>%
-        filter(Sex == "F") %>%
-        filter(WeightClassKg == 90) %>%
+openpl_tidy %>%
+        # filter(Tested == "Yes") %>%
+        filter(Sex == "M") %>%
+        filter(WeightClassKg == "82.5") %>%
+        select(Lift, Equipment, LiftKg) %>%
+na.omit() %>%
+        group_by(Lift, Equipment) %>%
+        summarise(meankg=mean(LiftKg),
+                  mediankg =median(LiftKg)) %>%
+        mutate(meanlbs = meankg*2.2046226218,
+               medianlbs = mediankg*2.2046226218) %>%
+        filter(Equipment == "Raw")
+
+       
                 
-        #         "Name", 
+                
+                
+                
+                
+                 #         "Name", 
         # "Sex", 
         # "Event", 
         # "Equipment", 
